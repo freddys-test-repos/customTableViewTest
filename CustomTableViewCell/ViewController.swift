2f50877188d51e9f502b8fae98285139aca8676d
amerino <alfimerino@gmail.com>
2019-04-09 22:56:10 -0400

    2nd commit

--- CustomTableViewCell/ViewController.swift
+++ CustomTableViewCell/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct CellData {
+    let image: UIImage
+    let message: String?
+}
 
+class TableViewController: UITableViewController {
+
+    var data = [CellData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        data = [CellData.init(image: #imageLiteral(resourceName: "Waterpic.jpg"), message: "WaterWaterWaterWaterWaterWaterWaterWaterWaterWaterWaterWaterWaterWater" ), CellData.init(image: #imageLiteral(resourceName: "Waterpic.jpg"), message: "Water" ), CellData.init(image: #imageLiteral(resourceName: "Waterpic.jpg"), message: "Water" )]
+        
+        self.tableView.register(CustomCell.self, forCellReuseIdentifier: "custom")
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 200
+        
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "custom") as! CustomCell
+        cell.mainImage = data[indexPath.row].image
+        cell.message = data[indexPath.row].message
+        return cell
+    }
+    
+    
 }
 
